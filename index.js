e4893e86707e59a0bb4521a2e7aaf745672950a3
Robbie Jaeger <jaeger.rob@gmail.com>
2019-04-03 09:45:44 -0600

    Scrape num kata completed

---
+++ index.js
@@ -0,0 +1,38 @@
+const Nightmare = require('nightmare');
+
+const studentKataURLs = require('./students.js');
+
+const goToPageAndGetData = async (pageLink) => {
+  try {
+    const nightmare = new Nightmare({ show: false });
+
+    const result = await nightmare
+      .goto(pageLink)
+      .wait('#shell_content')
+      .evaluate(() => {
+        const username = document.location.href.split('/').pop();
+
+        const stats = [...document.querySelectorAll('.stat-container .stat-box .stat')];
+        const numKataCompleted = stats.find(stat => stat.innerText.includes('Total Completed Kata')).innerText;
+
+        return {username, numKataCompleted};
+      })
+      .end();
+
+      return result;
+  } catch(e) {
+    console.error(e);
+  }
+};
+
+console.log(`Scraping katas...`);
+
+const data = studentKataURLs.reduce(async (acc, pageLink, idx) => {
+  const dataArray = await acc;
+  const {username, numKataCompleted} = await goToPageAndGetData(pageLink);
+
+  console.log(`${username}: ${numKataCompleted}`);
+  dataArray.push(numKataCompleted);
+  return dataArray;
+}, Promise.resolve([]));
+
---
+++ students.js
@@ -0,0 +1,4 @@
+module.exports = [
+  'https://www.codewars.com/users/mic80205',
+  'https://www.codewars.com/users/Rosebud303'
+];
