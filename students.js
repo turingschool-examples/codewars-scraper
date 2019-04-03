d651910be58c113c12359b12a5a16a8eeff2dcb0
Robbie Jaeger <jaeger.rob@gmail.com>
2019-04-03 13:46:10 -0600

    Add student to list

--- students.js
+++ students.js
@@ -3,6 +3,7 @@ module.exports = [
   'https://www.codewars.com/users/Rosebud303',
   'https://www.codewars.com/users/JustKamina',
   'https://www.codewars.com/users/rdren0',
+  'https://www.codewars.com/users/raechelo',
   'https://www.codewars.com/users/SallyHaefling',
   'https://www.codewars.com/users/JellyBeans1312',
   'https://www.codewars.com/users/B-Coyle',
