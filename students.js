dec3844198c03fe50bbe5fbbdbd6c19f891bd705
Brittany Storoz <brittanystoroz@gmail.com>
2019-08-04 16:19:24 -0600

    Add 1906 usernames

--- students.js
+++ students.js
@@ -2,26 +2,8 @@
 // the output will be in the order of the array
 
 module.exports = [
-  'https://www.codewars.com/users/ArAprojects',
-  'https://www.codewars.com/users/lundgrea',
-  'https://www.codewars.com/users/Asi5',
-  'https://www.codewars.com/users/aripp2',
-  'https://www.codewars.com/users/CervAnn',
-  'https://www.codewars.com/users/ayladharamsey',
-  'https://www.codewars.com/users/BrandyMello',
-  'https://www.codewars.com/users/bld010',
-  'https://www.codewars.com/users/Mr.BumDump',
-  'https://www.codewars.com/users/davidagitlen',
-  'https://www.codewars.com/users/djavan',
-  'https://www.codewars.com/users/EmilyLalonde',
-  'https://www.codewars.com/users/eoneill23',
-  'https://www.codewars.com/users/gregoryanderson',
-  'https://www.codewars.com/users/jogren',
-  'https://www.codewars.com/users/Jessiewithani',
-  'https://www.codewars.com/users/Triplemanus',
-  'https://www.codewars.com/users/julianenochs',
-  'https://www.codewars.com/users/kawilliams8',
-  'https://www.codewars.com/users/dawnlunacy',
-  'https://www.codewars.com/users/SamanthaLFreeman',
-  'https://www.codewars.com/users/yaleksandrova'
+  'https://www.codewars.com/users/ericwm76',
+  'https://www.codewars.com/users/KVeitch',
+  'https://www.codewars.com/users/posi7790',
+  'https://www.codewars.com/users/qfarenwald'
 ];
