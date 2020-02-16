b39678748e91904a362228b0bc3bd5f293c0e171
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-02-15 17:43:49 -0700

    Add scraper and data for M1 students

---
+++ m1-students.js
@@ -0,0 +1,9 @@
+// A list of student codewars profile URLs
+// the output will be in the order of the array
+
+let m1Students = [
+'https://www.codewars.com/users/CHaiz15',
+]
+
+
+module.exports = m1Students;
